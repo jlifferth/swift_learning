e6121f407ce9a64b2098141d87fe0b2321f8aa3d
Jonathan Lifferth <jonathan.lifferth@gmail.com>
2022-01-01 21:26:15 -0700

    initial commit

---
+++ CodeWithChris/Lesson7.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+// function practice
+
+var greeting = "Henlo, playground"
+print(greeting)
+
+func myFunc(a:Int, b:Int) -> Int {
+    return a + b
+}
+
+let response = myFunc(a: 2, b: 3)
+print(response)
