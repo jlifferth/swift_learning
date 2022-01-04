5ea36ca5b52a2ea1221e3dc52d5b7d04a5159df7
Jonathan Lifferth <jonathan.lifferth@gmail.com>
2022-01-04 07:49:17 -0700

    emergency save, xcode unresponsive

--- access_glucose/access_glucose/ContentView.swift
+++ access_glucose/access_glucose/ContentView.swift
@@ -6,11 +6,50 @@
 //
 
 import SwiftUI
+import HealthKit
+
+func fetchHealthData() -> Void {
+    let healthStore = HKHealthStore()
+    
+    if HKHealthStore.isHealthDataAvailable() {
+        let readData = Set([HKObjectType.quantityType(forIdentifier: .heartRate)!])
+        healthStore.requestAuthorization(toShare: [], read: readData) { (success, error) in
+            if success {
+                let calendar = NSCalendar.current
+
+                var anchorComponents = calendar.dateComponents([.day, .month, .year, .weekday], from: NSDate() as Date)
+
+                let offset = (7 + anchorComponents.weekday! - 2) % 7
+
+                anchorComponents.day! -= offset
+                anchorComponents.hour = 2
+                guard let anchorDate = Calendar.current.date(from: anchorComponents) else {
+                    fatalError("*** unable to create a valid date from the given components ***")
+                }
+            } else {
+                        print("Authorization failed")
+                }
+        }
+
+    } else {
+        print("No HealthKit data available")
+    }
+}
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button(action: fetchHealthData) {
+                Text("Fetch data")
+                .font(.largeTitle)
+                .bold()
+                .foregroundColor(.white)
+        }
+        .frame(width: 300, height: 150)
+                .background(Color.black)
+                .cornerRadius(40)
+                .border(Color.black)
+                .cornerRadius(40)
+                .padding()
     }
 }
 
